26c9e0acb00f9534271d0e974f54e936ed39b514
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-09-20 16:14:12 -0500

    Update EXPLORING BIKES_DW.sql

--- EXPLORING BIKES_DW.sql
+++ EXPLORING BIKES_DW.sql
@@ -1,3 +1,7 @@
 use BikesDW
 
 select top 5 * from DimProductCat_EXT
+
+select top 5 calendar_date, calendar_year, day_name from DimDate
+where calendar_year = 2015
+group by day_name, calendar_date, calendar_year
