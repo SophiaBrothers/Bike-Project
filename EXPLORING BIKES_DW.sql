269fd641c1923c8efb866460c93df9408cdf4ea9
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-09-21 12:45:11 -0500

    Update EXPLORING BIKES_DW.sql

--- EXPLORING BIKES_DW.sql
+++ EXPLORING BIKES_DW.sql
@@ -5,3 +5,17 @@ select top 5 * from DimProductCat_EXT
 select top 5 calendar_date, calendar_year, day_name from DimDate
 where calendar_year = 2015
 group by day_name, calendar_date, calendar_year
+
+select top 1 * from DimBusinessPartners
+
+--Counting columns in table
+SELECT count(*)
+FROM information_schema.columns
+WHERE table_name = 'DimBusinessPartners'
+
+
+--CHECKING ETL EXECUTION
+SELECT TOP 5 * FROM stagging_BusinessPartners
+
+--SUCCESS!!
+
