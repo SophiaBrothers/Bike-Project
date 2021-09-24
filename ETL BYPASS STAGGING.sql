261a9bbd2f33256f83d0e5b2d78000fbd6762e53
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-09-24 10:59:16 -0500

    Create ETL BYPASS STAGGING.sql

---
+++ ETL BYPASS STAGGING.sql
@@ -0,0 +1,20 @@
+--ETL PROCESS OF JOINING TWO TABLES AND BYPASSING STAGGING TABLES
+
+
+USE [BikesDB]
+GO
+
+--ETL SCRIPT
+
+-- Products_EXT
+select p.*, pt.LANGUAGE, pt.SHORT_DESCR, pt.MEDIUM_DESCR, pt.LONG_DESCR
+from Products p
+right outer join ProductTexts pt on p.PRODUCT_ID = pt.PRODUCT_ID;
+
+
+
+--ProductCat_EXT AS
+select pc.PRODCATEGORY_ID, pc.CREATEDBY, pc.CREATEDAT, pcs.LANGUAGE,
+pcs.SHORT_DESCR, pcs.MEDIUM_DESCR, pcs.LONG_DESCR
+from ProductCategories pc
+inner join ProductCategoryText pcs on pc.PRODCATEGORY_ID = pcs.PRODCATEGORY_ID
