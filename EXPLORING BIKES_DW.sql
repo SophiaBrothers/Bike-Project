48ed54e45a9cfdf7d608685bfa6e02f5662f0277
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-09-25 08:57:22 -0500

    Update EXPLORING BIKES_DW.sql

--- EXPLORING BIKES_DW.sql
+++ EXPLORING BIKES_DW.sql
@@ -8,16 +8,16 @@ group by day_name, calendar_date, calendar_year
 
 select top 1 * from DimBusinessPartners
 
---Counting columns in table
+/** Counting columns in table **/
 SELECT count(*)
 FROM information_schema.columns
 WHERE table_name = 'DimBusinessPartners'
 
 
---CHECKING ETL EXECUTION
+/** CHECKING ETL EXECUTION  **/
 SELECT TOP 5 * FROM stagging_BusinessPartners
 
---SUCCESS!!
+/** SUCCESS!! **/
 
 SELECT TOP 5 * FROM stagging_Addresses
 SELECT TOP 5 * FROM stagging_Employees
@@ -28,16 +28,16 @@ SELECT TOP 5 * FROM stagging_ProductTexts
 SELECT TOP 5 * FROM stagging_SalesOrderItems
 SELECT TOP 5 * FROM stagging_SalesOrders
 
---Will need to figure out how to tranfer data from multiple tables to each stagging table
--- without creating an ETL package for each
+/**  Will need to figure out how to tranfer data from multiple tables to each stagging table
+without creating an ETL package for each  **/
 
 
--- TO DELETE ALL RECORDS FROM A TABLE WITHOUT DELETING THE TABLE
+/**  TO DELETE ALL RECORDS FROM A TABLE WITHOUT DELETING THE TABLE  **/
 delete from stagging_Addresses
 
 select * from stagging_Addresses
 
---I created a new ETL package (SCD.address) that inludes a SCD element. So, when the stagging_address table is
---called, it will have a new validity start date that matches the date it was loaded into the DW.
+/**  I created a new ETL package (SCD.address) that inludes a SCD element. So, when the stagging_address table is
+called, it will have a new validity start date that matches the date it was loaded into the DW.  **/
 
 
