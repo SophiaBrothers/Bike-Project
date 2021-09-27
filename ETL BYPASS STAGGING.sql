07d9de0f559ab3e9638a19ced85999d3f3b4f14f
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-09-26 19:07:10 -0500

    Update ETL BYPASS STAGGING.sql

--- ETL BYPASS STAGGING.sql
+++ ETL BYPASS STAGGING.sql
@@ -2,7 +2,7 @@
 ETL: JOINING TWO TABLES AND BYPASSING STAGGING TABLES
 
 
-In SSIS, either use the views as source or utilize command statements below
+In SSIS, either use the views as source or utilize T-SQL command statements below
 
 products_Ext: type 2 
 
@@ -31,12 +31,23 @@ right outer join ProductTexts pt on p.PRODUCT_ID = pt.PRODUCT_ID;
 
 
 
-/**ProductCat_EXT**/
+--ProductCat_EXT
 select pc.PRODCATEGORY_ID, pc.CREATEDBY, pc.CREATEDAT, pcs.LANGUAGE,
 pcs.SHORT_DESCR, pcs.MEDIUM_DESCR, pcs.LONG_DESCR
 from ProductCategories pc
 inner join ProductCategoryText pcs on pc.PRODCATEGORY_ID = pcs.PRODCATEGORY_ID
 
+
+
+--The statement directly above will not work. It needs to be tweeted to convert INT to date for the
+--OLE DB Source element.  
+
+--Use this instead 
+
+select pc.PRODCATEGORY_ID, pc.CREATEDBY, CONVERT(date, CAST(pc.CREATEDAT AS char(8))) as CREATEDAT, pcs.LANGUAGE,
+pcs.SHORT_DESCR, pcs.MEDIUM_DESCR, pcs.LONG_DESCR
+from ProductCategories pc 
+inner join ProductCategoryText pcs on pc.PRODCATEGORY_ID = pcs.PRODCATEGORY_ID;
 
 
 --AFTER EXECUTING THE PACKAGE IN VISUAL STUDIO, PERFORM TEST
@@ -48,33 +59,10 @@ USE BikesDW
 SELECT * FROM DimProductCat_EXT
 
 
-
+--SUCCESS
 
 
 /*****************************************************************************************/
 
 
---Seeking ways to convert CREATEDAT column from int to date.
-
-
-SELECT COL_LENGTH('ProductCAT_EXT', 'CREATEDAT')  
---lenght of this column is 4. must be 8 for data type DT_DBDATE in SSIS
-
-
-select 
-CAST(CREATEDAT AS char(8))
-FROM ProductCat_EXT;
-
-
-
-select 
-CONVERT(date, CAST(CREATEDAT AS char(8)))
-FROM ProductCat_EXT;
-
-
-
-select pc.PRODCATEGORY_ID, pc.CREATEDBY, CONVERT(date, CAST(pc.CREATEDAT AS char(8))) as CREATEDAT, pcs.LANGUAGE,
-pcs.SHORT_DESCR, pcs.MEDIUM_DESCR, pcs.LONG_DESCR
-from ProductCategories pc 
-inner join ProductCategoryText pcs on pc.PRODCATEGORY_ID = pcs.PRODCATEGORY_ID;
 
