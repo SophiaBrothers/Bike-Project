d60293dcba8a186e2744ad177336419c4c2bfe76
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-09-28 11:34:38 -0500

    Update CREATE DIM & FACT TABLES.sql

--- CREATE DIM & FACT TABLES.sql
+++ CREATE DIM & FACT TABLES.sql
@@ -1,6 +1,7 @@
 USE [BikesDW]
 GO
 
+
 /****** Object:  Table [dbo].[Dim_Addresses]    Script Date: 9/11/2021 3:50:25 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -22,7 +23,7 @@ CREATE TABLE [dbo].[DimAddresses](
 	[VALIDITY_ENDDATE] [int] NULL,
 	[LATITUDE] [float] NULL,
 	[LONGITUDE] [float] NULL
-) ON [PRIMARY]
+) ON [PRIMARY];
 GO
 
 
@@ -49,14 +50,14 @@ CREATE TABLE [dbo].[DimBusinessPartners](
 	[CHANGEDBY] [nvarchar](50) NULL,
 	[CHANGEDAT] [int] NULL,
 	[CURRENCY] [nvarchar](50) NOT NULL
-) ON [PRIMARY]
+) ON [PRIMARY];
 GO
 
 
 
 
 /****** Object:  Table [dbo].[DimEmployees]    Script Date: 9/11/2021 4:03:06 PM ******/
--- REMOVE COLUMNS 14 THROUGH 19--
+-- REMOVED COLUMNS NAME INITIALS AND 14 THROUGH 19--
 
 SET ANSI_NULLS ON
 GO
@@ -70,7 +71,6 @@ CREATE TABLE [dbo].[DimEmployees](
 	[NAME_FIRST] [nvarchar](50) NOT NULL,
 	[NAME_MIDDLE] [nvarchar](50) NULL,
 	[NAME_LAST] [nvarchar](50) NOT NULL,
-	[NAME_INITIALS] [nvarchar](1) NULL,
 	[SEX] [nvarchar](50) NOT NULL,
 	[LANGUAGE] [nvarchar](50) NOT NULL,
 	[PHONENUMBER] [nvarchar](50) NOT NULL,
@@ -79,152 +79,8 @@ CREATE TABLE [dbo].[DimEmployees](
 	[ADDRESSID] [int] NOT NULL,
 	[VALIDITY_STARTDATE] [int] NOT NULL,
 	[VALIDITY_ENDDATE] [int] NOT NULL
-) ON [PRIMARY]
+) ON [PRIMARY];
 GO
-
-
-/****** Object:  Table [dbo].[FactSalesOrderItems]    Script Date: 9/11/2021 3:17:27 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[FactSalesOrderItems](
-	[ORDERITEM_KEY] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	[SALESORDER_ID] [int] NOT NULL,
-	[SALESORDERITEM] [int] NOT NULL,
-	[PRODUCT_ID] [nvarchar](50) NOT NULL,
-	[NOTE_ID] [nvarchar](1) NULL,
-	[CURRENCY] [nvarchar](50) NOT NULL,
-	[GROSSAMOUNT] [int] NOT NULL,
-	[NETAMOUNT] [float] NOT NULL,
-	[TAXAMOUNT] [float] NOT NULL,
-	[ITEMATPSTATUS] [nvarchar](50) NOT NULL,
-	[OPITEMPOS] [nvarchar](1) NULL,
-	[QUANTITY] [nvarchar](50) NOT NULL,
-	[QUANTITYUNIT] [nvarchar](50) NOT NULL,
-	[DELIVERYDATE] [int] NOT NULL
-) ON [PRIMARY]
-GO
-
-
-/****** Object:  Table [dbo].[FactSalesOrders]    Script Date: 9/11/2021 3:18:09 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[FactSalesOrders](
-	[SALESORDER_ID] [int] NOT NULL PRIMARY KEY,
-	[CREATEDBY] [nvarchar](50) NOT NULL,
-	[CREATEDAT] [int] NOT NULL,
-	[CHANGEDBY] [nvarchar](50) NOT NULL,
-	[CHANGEDAT] [int] NOT NULL,
-	[FISCVARIANT] [nvarchar](50) NOT NULL,
-	[FISCALYEARPERIOD] [int] NOT NULL,
-	[NOTE_ID] [nvarchar](1) NULL,
-	[PARTNER_ID] [int] NOT NULL,
-	[SALESORG] [nvarchar](50) NOT NULL,
-	[CURRENCY] [nvarchar](50) NOT NULL,
-	[GROSSAMOUNT] [int] NOT NULL,
-	[NETAMOUNT] [float] NOT NULL,
-	[TAXAMOUNT] [float] NOT NULL,
-	[LIFECYCLESTATUS] [nvarchar](50) NOT NULL,
-	[BILLINGSTATUS] [nvarchar](50) NOT NULL,
-	[DELIVERYSTATUS] [nvarchar](50) NOT NULL
-) ON [PRIMARY]
-GO
-
-/**SHOULD PROBABLY COMBINE PRODUCTCATEGORIES AND PRODUCTCATEGORYTEXT
-
-
-
-/****** Object:  Table [dbo].[DimProductCategories]    Script Date: 9/11/2021 3:19:22 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[DimProductCategories](
-	[PRODCATEGORY_ID] [nvarchar](50) NOT NULL,
-	[CREATEDBY] [int] NOT NULL,
-	[CREATEDAT] [int] NOT NULL
-) ON [PRIMARY]
-GO
-
-
-
-/****** Object:  Table [dbo].[DimProductCategoryText]    Script Date: 9/11/2021 3:19:51 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[DimProductCategoryText](
-	[PRODCATEGORY_ID] [nvarchar](50) NOT NULL,
-	[LANGUAGE] [nvarchar](50) NOT NULL,
-	[SHORT_DESCR] [nvarchar](50) NOT NULL,
-	[MEDIUM_DESCR] [nvarchar](1) NULL,
-	[LONG_DESCR] [nvarchar](1) NULL
-) ON [PRIMARY]
-GO
-
-
-
-/****** Object:  Table [dbo].[DimProducts]    Script Date: 9/11/2021 3:20:29 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[DimProducts](
-	[PRODUCT_ID] [nvarchar](50) NOT NULL,
-	[TYPECODE] [nvarchar](50) NOT NULL,
-	[PRODCATEGORY_ID] [nvarchar](50) NOT NULL,
-	[CREATEDBY] [nvarchar](50) NOT NULL,
-	[CREATEDAT] [int] NOT NULL,
-	[CHANGEDBY] [nvarchar](50) NOT NULL,
-	[CHANGEDAT] [int] NOT NULL,
-	[SUPPLIER_PARTNER_ID] [int] NOT NULL,
-	[TAXTARIFFCODE] [nvarchar](50) NOT NULL,
-	[QUANTITYUNIT] [nvarchar](50) NOT NULL,
-	[WEIGHTMEASURE] [float] NOT NULL,
-	[WEIGHTUNIT] [nvarchar](50) NOT NULL,
-	[CURRENCY] [nvarchar](50) NOT NULL,
-	[PRICE] [int] NOT NULL,
-	[WIDTH] [nvarchar](1) NULL,
-	[DEPTH] [nvarchar](1) NULL,
-	[HEIGHT] [nvarchar](1) NULL,
-	[DIMENSIONUNIT] [nvarchar](1) NULL,
-	[PRODUCTPICURL] [nvarchar](1) NULL
-) ON [PRIMARY]
-GO
-
-
-
-/****** Object:  Table [dbo].[DimProductTexts]    Script Date: 9/11/2021 3:21:17 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[DimProductTexts](
-	[PRODUCT_ID] [nvarchar](50) NOT NULL,
-	[LANGUAGE] [nvarchar](50) NOT NULL,
-	[SHORT_DESCR] [nvarchar](50) NOT NULL,
-	[MEDIUM_DESCR] [nvarchar](50) NULL,
-	[LONG_DESCR] [nvarchar](1) NULL
-) ON [PRIMARY]
-GO
-
-**/
-
 
 
 
@@ -274,28 +130,85 @@ CREATE TABLE [dbo].[DimProducts_EXT](
 	[WEIGHTUNIT] [nvarchar](50) NOT NULL,
 	[CURRENCY] [nvarchar](50) NOT NULL,
 	[PRICE] [int] NOT NULL,
-	[WIDTH] [nvarchar](1) NULL,
-	[DEPTH] [nvarchar](1) NULL,
-	[HEIGHT] [nvarchar](1) NULL,
-	[DIMENSIONUNIT] [nvarchar](1) NULL,
-	[PRODUCTPICURL] [nvarchar](1) NULL,
 	[LANGUAGE] [nvarchar](50) NOT NULL,
 	[SHORT_DESCR] [nvarchar](50) NOT NULL,
 	[MEDIUM_DESCR] [nvarchar](50) NULL,
 	[LONG_DESCR] [nvarchar](1) NULL
-) ON [PRIMARY]
+) ON [PRIMARY];
 GO
 
 
---For some reason, the code still created the tables that I didnt want.  Drop these tables
 
-DROP TABLE [dbo].[DimProductCategories];
-DROP TABLE [dbo].[DimProductCategoryText];
-DROP TABLE [dbo].[DimProducts];
-DROP TABLE [dbo].[DimProductTexts];
+/****** Object:  Table [dbo].[FactSalesOrderItems]    Script Date: 9/11/2021 3:17:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[FactSalesOrderItems](
+	[ORDERITEM_KEY] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[SALESORDER_ID] [int] NOT NULL,
+	[SALESORDERITEM] [int] NOT NULL,
+	[PRODUCT_KEY] [int] NOT NULL,
+	[PRODUCT_ID] [nvarchar](50) NOT NULL,
+	[CURRENCY] [nvarchar](50) NOT NULL,
+	[GROSSAMOUNT] [int] NOT NULL,
+	[NETAMOUNT] [float] NOT NULL,
+	[TAXAMOUNT] [float] NOT NULL,
+	[ITEMATPSTATUS] [nvarchar](50) NOT NULL,
+	[QUANTITY] [nvarchar](50) NOT NULL,
+	[QUANTITYUNIT] [nvarchar](50) NOT NULL,
+	[DELIVERYDATE] [int] NOT NULL
+) ON [PRIMARY];
+GO
+
+
+
+/****** Object:  Table [dbo].[FactSalesOrders]    Script Date: 9/11/2021 3:18:09 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[FactSalesOrders](
+	[SALESORDER_ID] [int] NOT NULL PRIMARY KEY,
+	[CREATEDBY] [nvarchar](50) NOT NULL,
+	[CREATEDAT] [int] NOT NULL,
+	[CHANGEDBY] [nvarchar](50) NOT NULL,
+	[CHANGEDAT] [int] NOT NULL,
+	[FISCVARIANT] [nvarchar](50) NOT NULL,
+	[FISCALYEARPERIOD] [int] NOT NULL,
+	[PARTNER_KEY] [int] NOT NULL,
+	[PARTNER_ID] [int] NOT NULL,
+	[SALESORG] [nvarchar](50) NOT NULL,
+	[CURRENCY] [nvarchar](50) NOT NULL,
+	[GROSSAMOUNT] [int] NOT NULL,
+	[NETAMOUNT] [float] NOT NULL,
+	[TAXAMOUNT] [float] NOT NULL,
+	[LIFECYCLESTATUS] [nvarchar](50) NOT NULL,
+	[BILLINGSTATUS] [nvarchar](50) NOT NULL,
+	[DELIVERYSTATUS] [nvarchar](50) NOT NULL
+) ON [PRIMARY];
+GO
 
 
 -------------------------------------------------------------------------------------------------------------------
-/** ADD FORIEGN KEYS **/
+/** CREATE FORIEGN KEYS **/
 -------------------------------------------------------------------------------------------------------------------
 
+-- adding to FactSalesOrderItems
+ALTER TABLE [dbo].[FactSalesOrderItems]  WITH CHECK ADD  CONSTRAINT [FK_Prod_SOI] FOREIGN KEY([PRODUCT_KEY])
+REFERENCES [dbo].[DimProducts_EXT] ([PRODUCT_KEY])
+
+ALTER TABLE [dbo].[FactSalesOrderItems] CHECK CONSTRAINT [FK_Prod_SOI];
+GO
+
+
+-- adding to FactSalesOrders
+ALTER TABLE [dbo].[FactSalesOrders]  WITH CHECK ADD  CONSTRAINT [FK_Part_SOI] FOREIGN KEY([PARTNER_KEY])
+REFERENCES [dbo].[DimBusinessPartners] ([PARTNER_KEY])
+
+ALTER TABLE [dbo].[FactSalesOrders] CHECK CONSTRAINT [FK_Part_SOI];
+GO
