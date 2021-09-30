25e524534a6ac603ad1ac5205c277881c0fc6aca
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-09-29 22:26:24 -0500

    Create CLEANING DATA FIELDS.sql

---
+++ CLEANING DATA FIELDS.sql
@@ -0,0 +1,43 @@
+﻿/***********************   DATA CLEANING  *************************************/
+USE BikesDB
+
+Select * from Employees
+
+
+--Problem columns are:  NAME_FIRST,   NAME_LAST,   PHONENUMBER,   VALIDITY_STARTDATE,   VALIDITY_ENDATE
+
+------------------------------------------------------------------------------------------------
+
+
+--CLEANING NAME_FIRST & NAME_LAST 
+
+--A diamond (or square) with a question mark in the middle. This is a "replacement character."
+--It is displayed whenever a character is not recognized in a document or webpage                        
+
+
+
+--replace special character
+
+--REPLACE(string, old_string, new_string)
+
+select NAME_FIRST, REPLACE(NAME_FIRST COLLATE Latin1_General_BIN, N'�','e') as CLEAN_NAME from Employees;
+select NAME_LAST, REPLACE(NAME_LAST COLLATE Latin1_General_BIN, N'�','e') as CLEAN_NAME from Employees;
+
+--COLLATE is used to cast the string or column collation into a specified collation; In this case, Latin1_General_BIN 
+--The N denotes that the subsequent string is in Unicode
+
+
+--to update in table
+begin transaction
+update Employees 
+set NAME_FIRST = REPLACE(NAME_FIRST COLLATE Latin1_General_BIN,N'�','e')
+
+update Employees 
+set NAME_LAST = REPLACE(NAME_LAST COLLATE Latin1_General_BIN,N'�','e')
+
+
+rollback;
+
+
+
+
